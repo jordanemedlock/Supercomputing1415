4a734035108df74d6e5349c60129777504f3711e
samgervais <samgervais512@gmail.com>
2015-01-30 14:49:52 -0700

    More fun with the search bar!

--- HTML/index.js
+++ HTML/index.js
@@ -2,10 +2,23 @@ $(document).ready(function() {
   $("#search").change(function() {
   	var search=$("#search").val()
   	if(search == "doge") {
-  		window.open(["http://weknowmemes.com/2013/11/best-of-the-doge-meme-15-pics/"]);
+  		window.open(["http://weknowmemes.com/2013/11/best-of-the-doge-meme-15-pics/"])
   	}
-  	else if(search == "back in the day" || search == "back in nam" || search == "LOL") {
-  		window.open(["https://twitter.com/jordanemedlock/status/126382915285291009"]);
+  	else if(search == "back in the day" || search == "lel" || search == "LOL") {
+  		var random = parseInt(Math.random()*10)
+  		var twitter = [
+  		"https://twitter.com/jordanemedlock/status/126382915285291009",
+  		"https://twitter.com/jordanemedlock/status/230350738839187458",
+  		"https://twitter.com/jordanemedlock/status/137402194256338946",
+  		"https://twitter.com/jordanemedlock/status/88816149214658560",
+  		"https://twitter.com/jordanemedlock/status/112760542271242240",
+  		"https://twitter.com/jordanemedlock/status/115611206563270656",
+  		"https://twitter.com/jordanemedlock/status/110931996750004226",
+		"https://twitter.com/jordanemedlock/status/114081908358512640",
+		"https://twitter.com/jordanemedlock/status/92490374521683968",
+  		"http://jordanemedlock.com"
+  		]
+  		window.open([twitter[random]])
   	}
   })
 })
