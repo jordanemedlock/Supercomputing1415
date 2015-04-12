4f2015ea9c2f2d9ee662ff330ea424144d34cf15
Jordan Medlock <jordanemedlock@gmail.com>
2015-04-12 14:03:28 -0600

    Some Stuff

--- HTML/index.js
+++ HTML/index.js
@@ -10,15 +10,6 @@ $(document).ready(function() {
   	else if(search == "back in the day" || search == "lel" || search == "LOL") {
   		var random = Math.floor(Math.random()*10);
   		var twitter = [
-  		"https://twitter.com/jordanemedlock/status/126382915285291009",
-  		"https://twitter.com/jordanemedlock/status/230350738839187458",
-  		"https://twitter.com/jordanemedlock/status/137402194256338946",
-  		"https://twitter.com/jordanemedlock/status/88816149214658560",
-  		"https://twitter.com/jordanemedlock/status/112760542271242240",
-  		"https://twitter.com/jordanemedlock/status/115611206563270656",
-  		"https://twitter.com/jordanemedlock/status/110931996750004226",
-		  "https://twitter.com/jordanemedlock/status/114081908358512640",
-		  "https://twitter.com/jordanemedlock/status/92490374521683968",
   		"http://jordanemedlock.com"
   		];
   		window.open([twitter[random]]);
@@ -26,6 +17,7 @@ $(document).ready(function() {
   	else {
   		$.getJSON("/search/" + encodeURIComponent(search), function(data) {
         console.log(data);
+        $("#output").empty();
         for (var i = data.length - 1; i >= 0; i--) {
           console.log(data[i]);
           $("#output").prepend('<img src="' + data[i] + '"/>');
